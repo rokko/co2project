715951b3e778ee8dd21c80ab66133f345376283a
Rocco Caricola <rocco.caricola89@gmail.com>
2022-10-21 19:16:45 +0200

    nuovo design

--- src/faq/Faq.tsx
+++ src/faq/Faq.tsx
@@ -1,11 +1,70 @@
+import { Divider } from "@mui/material";
 import React from "react";
 import { TitleSection } from "../co2listing/Co2ListingStyled";
+import { ContainerFaq, TestoDomandaFaq } from "./FaqStyled";
+import { Collapse } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Faq = () => {
   return (
-    <>
-      <TitleSection>FAQ</TitleSection>
-    </>
+    <ContainerFaq>
+      <TitleSection style={{ textAlign: "left" }}>FAQ</TitleSection>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="first-header"
+        >
+          <TestoDomandaFaq>
+            Perchè è stata adottata la tecnologia blockchain?
+          </TestoDomandaFaq>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="second-header"
+        >
+          <TestoDomandaFaq>
+            Quale tecnologia blockchain è stata scelta?
+          </TestoDomandaFaq>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="third-header"
+        >
+          <TestoDomandaFaq>
+            Cosa può fare l'utente, una volta acquistato il/i credito/i?
+          </TestoDomandaFaq>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </ContainerFaq>
   );
 };
 
