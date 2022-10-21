ad24001cf1e8a157016d32933a4263a99e9dd897
Rocco Caricola <rocco.caricola89@gmail.com>
2022-10-21 19:16:07 +0200

    faq

--- src/faq/FaqStyled.tsx
+++ src/faq/FaqStyled.tsx
@@ -1,1 +1,20 @@
-export {};
+import styled from "styled-components";
+
+const TestoDomandaFaq = styled.p`
+  font-size: 28px;
+  font-weight: bold;
+`;
+
+const TestoRispostaFaq = styled.p`
+  font-size: 28px;
+`;
+
+const ContainerFaq = styled.div`
+  width: 100vw;
+  padding: 8rem;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+
+export { ContainerFaq, TestoDomandaFaq, TestoRispostaFaq };
